7779b083252e1a84cc28525f66f1929a2897b0be
Yurii Malchevskyi <ymalchevskyi@qualtrics.com>
2026-06-13 11:54:00 +0200

    changed group to show days from start

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -344,7 +344,11 @@ export default function GroupScreen({
                       day: "numeric",
                     })}
                   >
-                    (i)
+                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                      <circle cx="12" cy="12" r="10"/>
+                      <line x1="12" y1="16" x2="12" y2="12"/>
+                      <line x1="12" y1="8" x2="12.01" y2="8"/>
+                    </svg>
                   </span>
                 </p>
               </div>
